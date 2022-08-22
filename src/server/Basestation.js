7a237be9e3ad25168cd37f27229bb892dae4f8de
ramamimu <ahhmaad.blogger@gmail.com>
2022-08-22 23:08:39 +0700

    init: program bs2pc pc2bs

--- src/server/Basestation.js
+++ src/server/Basestation.js
@@ -5,11 +5,12 @@ class Basestation {
   // general variable
   host = "0.0.0.0";
   group = "224.16.32.80";
-  udp_socket;
-  port_tx;
-  port_rx = "1111";
-  web_socket;
-  port_web_socket = "3000";
+  udp_socket_rx;
+  udp_socket_tx;
+  port_rx = "5656";
+  port_tx = "5666";
+  web_socket_;
+  port_web_socket = "3002";
   pc2bs_data = {};
   bs2pc_data = {};
 
@@ -31,10 +32,11 @@ class Basestation {
     });
 
     // udp
-    this.udp_socket = require("dgram").createSocket("udp4");
+    this.udp_socket_rx = require("dgram").createSocket("udp4");
+    this.udp_socket_tx = require("dgram").createSocket("udp4");
     this.buffer = require("buffer").Buffer;
   }
-  getPc2Bs() {
+  getPc2BsData(message) {
     let counter = 0;
     try {
       this.pc2bs_data.header = [
@@ -43,39 +45,40 @@ class Basestation {
         String.fromCharCode(message[2]),
       ];
       counter = 3;
-      this.pc2bs_data.pos_x = message.readInt16LE(3); //pos x
+      this.pc2bs_data.pos_x = message.readInt16LE(counter); //pos x
       counter += 2;
-      this.pc2bs_data.pos_y = message.readInt16LE(5); //pos y
+      this.pc2bs_data.pos_y = message.readInt16LE(counter); //pos y
       counter += 2;
-      this.pc2bs_data.theta = message.readInt16LE(7); //theta
+      this.pc2bs_data.theta = message.readInt16LE(counter); //theta
       counter += 2;
-      this.pc2bs_data.status_bola = message.readUint8(9); //bola x pada lapangan
+      this.pc2bs_data.status_bola = message.readUint8(counter); //bola x pada lapangan
       counter += 1;
-      this.pc2bs_data.bola_x = message.readInt16LE(10); //bola y pada lapangan
+      this.pc2bs_data.bola_x = message.readInt16LE(counter); //bola y pada lapangan
       counter += 2;
-      this.pc2bs_data.bola_y = message.readInt16LE(12); //bola y pada lapangan
+      this.pc2bs_data.bola_y = message.readInt16LE(counter); //bola y pada lapangan
       counter += 2;
-      this.pc2bs_data.mcl_x = message.readInt16LE(14); //mcl x
+      this.pc2bs_data.mcl_x = message.readInt16LE(counter); //mcl x
       counter += 2;
-      this.pc2bs_data.mcl_y = message.readInt16LE(16); //mcl y
+      this.pc2bs_data.mcl_y = message.readInt16LE(counter); //mcl y
       counter += 2;
-      this.pc2bs_data.mcl_theta = message.readInt16LE(18); //mcl theta
+      this.pc2bs_data.mcl_theta = message.readInt16LE(counter); //mcl theta
       counter += 2;
-      this.pc2bs_data.robot_condition = message.readInt16LE(20); //robot condition
+      this.pc2bs_data.robot_condition = message.readInt16LE(counter); //robot condition
       counter += 2;
-      this.pc2bs_data.status_algoritma = message.readUint16LE(22); //status algoritma
+      this.pc2bs_data.status_algoritma = message.readUint16LE(counter); //status algoritma
       counter += 2;
-      this.pc2bs_data.status_sub_algoritma = message.readUint16LE(24); //status sub algoritma
+      this.pc2bs_data.status_sub_algoritma = message.readUint16LE(counter); //status sub algoritma
       counter += 2;
-      this.pc2bs_data.status_sub_sub_algoritma = message.readUint16LE(26); //status sub sub algoritma
+      this.pc2bs_data.status_sub_sub_algoritma = message.readUint16LE(counter); //status sub sub algoritma
       counter += 2;
-      this.pc2bs_data.status_sub_sub_sub_algoritma = message.readUint16LE(28); //status sub sub sub algoritma
+      this.pc2bs_data.status_sub_sub_sub_algoritma =
+        message.readUint16LE(counter); //status sub sub sub algoritma
       counter += 2;
-      this.pc2bs_data.target_umpan = message.readUint8(30); //target umpan
+      this.pc2bs_data.target_umpan = message.readUint8(counter); //target umpan
       counter += 1;
-      this.pc2bs_data.epoch = message.readUint32LE(31); //epoch
+      this.pc2bs_data.epoch = message.readUint32LE(counter); //epoch
       counter += 4;
-      this.pc2bs_data.stm_epoch = message.readUint32LE(35); //stm epoch
+      this.pc2bs_data.stm_epoch = message.readUint32LE(counter); //stm epoch
       counter += 4;
       // let counter = 39;
       this.pc2bs_data.obs = [];
@@ -84,10 +87,12 @@ class Basestation {
         counter += 2;
       }
       console.log(this.pc2bs_data);
-      // io.emit("sub message", this.pc2bs_data);
     } catch (e) {
       console.log("error range");
     }
+  }
+  sendDataToUI() {
+    this.web_socket.emit("sub message", this.pc2bs_data);
   }
   sayHello() {
     console.log("hello world");
--- src/server/index.js
+++ src/server/index.js
@@ -3,29 +3,67 @@ const Basestation = require("./Basestation");
 const HOST = Basestation.host;
 const GROUP = Basestation.group;
 const PORT_RX = Basestation.port_rx;
-const UDP_SOCKET = Basestation.udp_socket;
+const PORT_TX = Basestation.port_tx;
+const UDP_SOCKET_RX = Basestation.udp_socket_rx;
+const UDP_SOCKET_TX = Basestation.udp_socket_tx;
+const WEB_SOCKET = Basestation.web_socket;
 
-UDP_SOCKET.on("listening", function () {
-  var address = UDP_SOCKET.address();
-  console.log(
-    "UDP Client listening on " + address.address + ":" + address.port
-  );
-  UDP_SOCKET.setBroadcast(true);
-  UDP_SOCKET.setMulticastTTL(64);
-  UDP_SOCKET.addMembership(GROUP, HOST);
+// listening
+
+UDP_SOCKET_RX.on("listening", function () {
+  var address = UDP_SOCKET_RX.address();
+  console.log("UDP RX listening on " + address.address + ":" + address.port);
+  UDP_SOCKET_RX.setBroadcast(true);
+  UDP_SOCKET_RX.setMulticastTTL(64);
+  UDP_SOCKET_RX.addMembership(GROUP, HOST);
 });
 
-UDP_SOCKET.bind(PORT_RX, HOST, () => {
+UDP_SOCKET_TX.on("listening", function () {
+  var address = UDP_SOCKET_TX.address();
+  console.log("UDP TX listening on " + address.address + ":" + address.port);
+  UDP_SOCKET_TX.setBroadcast(true);
+  UDP_SOCKET_TX.setMulticastTTL(64);
+  UDP_SOCKET_TX.addMembership(GROUP, HOST);
+});
+
+// binding
+
+UDP_SOCKET_RX.bind(PORT_RX, HOST, () => {
   console.log(`udp ${HOST} connected`);
-  UDP_SOCKET.on("message", (message, remote) => {
-    console.log(
-      new Date().getTime() +
-        " \nB: data From UDP group pc2Bs: " +
-        remote.address +
-        ":" +
-        remote.port +
-        " - " +
-        message
-    );
-  });
 });
+
+UDP_SOCKET_TX.bind(PORT_TX, HOST, () => {
+  console.log(`udp ${HOST} connected`);
+});
+
+// on message
+
+UDP_SOCKET_RX.on("message", (message, remote) => {
+  console.log(
+    new Date().getTime() +
+      " \nB: data From UDP group pc2Bs: " +
+      remote.address +
+      ":" +
+      remote.port
+  );
+  //   + " - " + message
+  Basestation.getPc2BsData(message);
+  Basestation.sendDataToUI();
+});
+
+// UDP_SOCKET_TX.on("message", (message, remote) => {
+//   console.log(
+//     new Date().getTime() +
+//       " \nB: data From UDP group pc2Bs: " +
+//       remote.address +
+//       ":" +
+//       remote.port +
+//       " - " +
+//       message
+//   );
+// });
+
+setInterval(() => {
+  const setering = "ini sebuah string";
+  UDP_SOCKET_TX.send(setering, 0, setering.length, PORT_TX, GROUP);
+}, 25);
