4d63268b992a634f5d209176afe5e6776dc91bcc
Yuniarrr <mnynoname14@gmail.com>
2023-02-28 21:59:23 +0700

    feat: refbox 2019
    fix: command widget

--- src/App.vue
+++ src/App.vue
@@ -94,7 +94,12 @@ export default {
       if (refbox.status && !overrid_mode) {
         let translattorCommand = THAT.translateCommand(command);
         if (translattorCommand) {
-          if (target == Config.group_multicast || target == "") {
+          if (
+            target == Config.group_multicast ||
+            target == "" ||
+            target == "ALL" ||
+            target == "HOME"
+          ) {
             THAT.ROBOT_STATE.setCommand(translattorCommand);
           } else {
             THAT.ROBOT_STATE.setCommand(translattorCommand.toLowerCase());
